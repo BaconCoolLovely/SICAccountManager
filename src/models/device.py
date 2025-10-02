615b70dc0ad00c6ef077c8fb8b612fa7921a46e3
Royies <133587185+BaconCoolLovely@users.noreply.github.com>
2025-10-02 13:36:08 +0100

    Update device.py

--- src/models/device.py
+++ src/models/device.py
@@ -1,1 +1,10 @@
-# Device Model goes here
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from .user import Base
+
+class Device(Base):
+    __tablename__ = "devices"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    public_key = Column(String, nullable=True)
+    authorized = Column(Boolean, default=False)
